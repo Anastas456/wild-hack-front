34f1c25542f91006eac6f112eb52713187592c75
Anastas456 <wild456west@gmail.com>
2021-12-04 17:31:46 +0300

    основные компоненты и шрифты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './shared/components/main-header/main-header.component';
+import { CordonsComponent } from './cordons/cordons.component';
+import { AboutVolunteersComponent } from './about-volunteers/about-volunteers.component';
+import { InfoComponent } from './info/info.component';
+import { VolunteerFormComponent } from './volunteer-form/volunteer-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainHeaderComponent
+    MainHeaderComponent,
+    CordonsComponent,
+    AboutVolunteersComponent,
+    InfoComponent,
+    VolunteerFormComponent
   ],
   imports: [
     BrowserModule,
