e31115c0b64618527e076e509bb8cac04d3a7e99
Anastas456 <wild456west@gmail.com>
2021-12-04 22:52:36 +0300

    страница с информацией

---
+++ src/app/cordons/cordons.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CordonsRoutingModule } from './cordons-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CordonsRoutingModule
+  ]
+})
+export class CordonsModule { }
